9aa0b417b7da85e748d45a22d27cdc9ad33f6620
Nima Janbaz <mbnima1382@gmail.com>
2022-12-08 15:28:00 +0330

    end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+// import { ethers } from "ethers";
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+// import Alert from '@mui/material/Alert';
+import { useMoralis } from "react-moralis";
 
 function App() {
+
+  const { isAuthenticated, authenticate } = useMoralis()
+  const walletconnectAuth = async () => {
+    try {
+      await authenticate({
+        provider: "walletconnect",
+        signingMassage: "Connect With Localhost By ReactJs",
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+        <Grid item xs={3}>
+          {!isAuthenticated ? (
+            <Button onClick={walletconnectAuth} variant="contained">Login & Save ETH Address</Button>
+          ) : (
+            <>
+              <p className="text-gray-600">Welcom</p>
+            </>
+          )}
+          {/* {networkVersion == 1 ? (
+            <>
+              <p className="text-gray-600">Address: {wallet.address}</p>
+              <p className="text-gray-600">ETH Balance: {balanse.Balance}</p>
+              <p className="text-gray-600">Network: {network.network}</p>
+              <Button onClick={logout} variant="contained">LogOut</Button>
+              </>
+          ) : (
+            <Alert severity="error" sx={{ mt: "1rem" }}>Pleas Use Ethereum Mainnet In Your Wallet</Alert>
+          )} */}
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const envData = {
+  APP_ID: "QTVKCqekzskve1mpuq6ojOmB82DaBbfzJ9J6Uoqn",
+  SERVER_URL: "https://7sqjov2chgnu.usemoralis.com:2053/server"
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={envData.APP_ID} serverUrl={envData.SERVER_URL}>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
